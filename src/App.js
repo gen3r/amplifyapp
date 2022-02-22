dd54c5f0c8aa2f8ab0048c9a15cae41c12ab2112
gener <humanalmostco@gmail.com>
2022-02-21 22:38:28 -0500

    changes for v3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-// import anime from 'animejs/lib/anime.es.js';
 import logo from './logo.svg';
 import gif1 from './gifs/gif1.gif';
 import gif2 from './gifs/gif2.gif';
 import './App.css';
+import React from 'react';
+// import anime from 'animejs'
 
 function App() {
   return (
@@ -16,15 +16,14 @@ function App() {
       </header>
       <body className="App-body">
         <img src={gif1} className="gif-1" alt="gif"/>
+        <h3 className='bruh'>bruh website fr on rn ???
+          <span role="img" aria-label='confused'>😕😕😕</span> 
+        </h3>
+        <img src={gif2} className="gif-2" alt="gif2" />
         <h1>bruh website fr on rn 
           <span role="img" aria-label='thumbs up'>👍👍👍</span>
         </h1>
-        <img src={gif2} className="gif-2" alt="gif2" />
-        <h1>bruh website fr on rn ???
-          <span role="img" aria-label='confused'>😕😕😕</span> 
-        </h1>
       </body>
-
     </div>
   );
 }
